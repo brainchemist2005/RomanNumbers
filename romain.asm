2e6a91b998a37d82d9e1dd0c870154010bde4c54
Bouargan <bouargan.zakariae@ens.uqam.ca>
2023-10-04 21:00:29 -0400

    I finished the nummeral computation

--- romain.asm
+++ romain.asm
@@ -6,20 +6,19 @@
 
 .text
     li s1, 0
-    li t0,0   
+    li t0, 0
 
 loop:
-
     li a7, ReadChar
     ecall
-    li t3 , -1
-    beq a0,t3,done
+    li t3, -1
+    beq a0, t3, done
 
     li t2, 'M'
     beq a0, t2, isM
     li t2, 'D'
     beq a0, t2, isD
-    li t2, 'C'       
+    li t2, 'C'
     beq a0, t2, isC
     li t2, 'L'
     beq a0, t2, isL
@@ -29,53 +28,53 @@ loop:
     beq a0, t2, isV
     li t2, 'I'
     beq a0, t2, isI
-    j done  
+    j done
 
 isM:
     la t1, M_val
-    j checkGreater
-
+    j processNumeral
 isD:
     la t1, D_val
-    j checkGreater
-
+    j processNumeral
 isC:
     la t1, C_val
-    j checkGreater
-
+    j processNumeral
 isL:
     la t1, L_val
-    j checkGreater
-    
+    j processNumeral
 isX:
     la t1, X_val
-    j checkGreater
-
+    j processNumeral
 isV:
     la t1, V_val
-    j checkGreater
-
+    j processNumeral
 isI:
     la t1, I_val
-    j checkGreater
+    j processNumeral
 
-checkGreater:
-    lw t1, 0(t1)
-    bge t0, t1, addition # the problem is in t1
-    sub s1, s1, t0
-    sub t4,t1 , t0 
-    add s1, s1, t4
-    mv t0, t1
+processNumeral:
+    lw t2, 0(t1)
+    li a7, PrintInt
+    mv a0,s1
+    ecall
+    beq t0, zero, first
+    blt t0, t2, subtract
+    add s1, s1, t2
+    j updatePrevious
     
+first:
+    add s1,s1,t2
+    j updatePrevious
+
+subtract:
+    sub s1, s1, t0
+    sub t4,t2 , t0
+    add s1, s1, t4
+    j updatePrevious
+
+updatePrevious:
+    mv t0, t2  
     j loop
-
-
-addition:
-    lw t1, 0(t1)
-    add s1, s1, t1  
-    mv t0, t1        
-    j loop
-
 
 done:
     li a7, PrintInt
@@ -84,7 +83,14 @@ done:
     li a7, Exit
     ecall
 
+err:
+    li a7, PrintString
+    lw a0, error
+    ecall
+    j done
+
 .data
+error: .string "err"
 M_val: .word 1000
 D_val: .word 500
 C_val: .word 100
